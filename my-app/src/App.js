8205571635da890e6a1941037c8560a586f44784
noahtz <t0527184022@gmail.com>
2024-05-06 13:21:33 +0300

    add helping files

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-import './App.css';
+import './cssFiles/App.css';
 
 function ElevatorChallenge() {
   return (
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './cssFiles/index.css';
 import ElevatorChallenge from './App';
 import reportWebVitals from './reportWebVitals';
 
