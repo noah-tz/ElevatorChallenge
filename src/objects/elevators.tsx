a599cc541e55b6da28f2c7102f99117233b25aea
noahtz <t0527184022@gmail.com>
2024-05-08 14:51:45 +0300

    Rerouting image

--- src/objects/elevators.tsx
+++ src/objects/elevators.tsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import * as Stiles from '../stylesFiles/styles.ts';
 import settings from '../settings.ts';
-import elv from './elv.png'
+import elevatorImage from './elv.png'
 
 class Elevators extends React.Component {
     render() {
-        // const pathImage: string = '../../images/elv.png'
         const pathImage: string = './elv.png'
         return(
             <Stiles.Elevators>
                 {Array(settings.numOfElevators).fill(null).map((_, idx) => (
-                    <Stiles.Elevator src={elv} alt={`Elevator number ${idx +1}`}/>
-                    // <img src={elv} height={200} width={200} alt={`Elevator number ${idx +1}`}/>
+                    <Stiles.Elevator src={elevatorImage} alt={`Elevator number ${idx +1}`}/>
                 ))}
             </Stiles.Elevators>
         )
